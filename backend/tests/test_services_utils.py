fae2e89814e7c0fcf4ddf6d12c95b12678e6afb5
Ivan Zaycev <ivzaycev0717@yandex.ru>
2025-03-11 16:36:05 +0300

    tests done

--- backend/tests/test_services_utils.py
+++ backend/tests/test_services_utils.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 
+import pytest
 import pytz
 
-from services.utils import get_current_date
+from services.utils import compare_comments, get_current_date
 from settings import TIMEZONE
 
 
@@ -11,3 +12,11 @@ def test_get_current_date():
     assert isinstance(current_date, datetime)
     assert current_date.tzinfo.zone == pytz.timezone(TIMEZONE).zone
     assert current_date <= datetime.now(pytz.timezone(TIMEZONE))
+
+
+@pytest.mark.parametrize(
+        ('old', 'new', 'result'),
+        [('Abcd', 'Abcd', False),
+         ('abcd', 'abCd', True)])
+def test_compare_comments(old: str, new: str, result: bool):
+    assert compare_comments(old, new) is result

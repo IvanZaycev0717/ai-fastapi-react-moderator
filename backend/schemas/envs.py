b556d345fbcd16ed5884f9b95adf10b8edd5f69f
Ivan Zaycev <ivzaycev0717@yandex.ru>
2025-03-14 14:15:03 +0300

    pydantic-settings changed env

--- backend/schemas/envs.py
+++ backend/schemas/envs.py
@@ -1,6 +1,10 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from settings import ENVIROMENT_FILE
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=ENVIROMENT_FILE)
+
     yc_folder_id: str
     yc_secret_key: str
